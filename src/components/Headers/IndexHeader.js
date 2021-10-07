66b794d875a97c834ad0d914a91a2cd84b630dcb
Steven Yang <stevenyang@Steven-MacBook-Pro.local>
2021-10-07 19:37:44 -0400

    updatd date string

--- src/components/Headers/IndexHeader.js
+++ src/components/Headers/IndexHeader.js
@@ -8,6 +8,8 @@ import mobileBackground from 'assets/img/mobile-bg1.gif'
 import wideBackground from 'assets/img/wide-background2.gif'
 import {useMediaQuery} from 'react-responsive';
 import Countdown from 'react-countdown';
+import Moment from 'react-moment';
+import moment from 'moment';
 
 function IndexHeader() {
   const isDesktopOrLaptop = useMediaQuery({
@@ -20,6 +22,7 @@ function IndexHeader() {
   let pageHeader = React.createRef();
   var backgroundImagePath = require("assets/img/mobile-background.gif");
   const imageUrl = isTabletOrMobile ? mobileBackground : wideBackground;
+  
   React.useEffect(() => {
     if (window.innerWidth > 991) {
       const updateScroll = () => {
@@ -48,7 +51,7 @@ function IndexHeader() {
         <Container>
         <div className="space-50"></div>
           <Countdown
-              date={new Date("10-13-2021 22:00:00 UTC")}
+              date={"2021-10-13T22:00:00.000+00:00"}
               renderer={props => <div><h2 style={{'fontWeight':'bold'}}>Public Sale 10/13 10PM UTC(6PM EST)</h2><p style={{'fontWeight':'bold'}}>{props.formatted.days} day(s) {props.formatted.hours} hour(s) {props.formatted.minutes} minute(s) {props.formatted.seconds} second(s)</p></div>}
           />
           <div className="category category-absolute">
